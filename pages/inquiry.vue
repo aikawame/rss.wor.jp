c81b25e3b89af2d57bca0153333a9d3662a45c75
Masahiro Aikawa <aikawame@wor.jp>
2018-09-09 05:31:04 +0900

    お問い合わせのレイアウトを追加する

---
+++ pages/inquiry.vue
@@ -0,0 +1,54 @@
+<template>
+  <article id="inquiry" class="page">
+    <section class="hero is-primary">
+      <div class="hero-body has-text-centered">
+        <h1 class="title has-text-weight-normal">
+          <span class="has-text-weight-normal">お問い合わせ</span>
+        </h1>
+      </div>
+    </section>
+    <section class="contents">
+      <section class="ad container">
+        <adsbygoogle ad-slot="5977540638" />
+      </section>
+      <section class="form container">
+        <dl>
+          <div class="field is-horizontal">
+            <dt class="field-label is-normal">
+              <label class="label">お名前</label>
+            </dt>
+            <dd class="field-body control">
+              <input class="input" type="text" placeholder="任意でご入力ください" />
+            </dd>
+          </div>
+          <div class="field is-horizontal">
+            <dt class="field-label is-normal">
+              <label class="label">メールアドレス</label>
+            </dt>
+            <dd class="field-body control">
+              <input class="input" type="text" placeholder="返信をご希望の場合はご入力ください" />
+            </dd>
+          </div>
+          <div class="field is-horizontal">
+            <dt class="field-label is-normal">
+              <label class="label">内容</label>
+            </dt>
+            <dd class="field-body control">
+              <textarea class="textarea" placeholder="お問い合わせ内容をご入力ください"></textarea>
+            </dd>
+          </div>
+          <div class="field is-horizontal">
+            <dt class="field-label is-normal">
+            </dt>
+            <dd class="field-body control">
+              <button class="button is-primary is-fullwidth">送信</button>
+            </dd>
+          </div>
+        </dl>
+      </section>
+      <section class="ad container">
+        <adsbygoogle ad-slot="8622502386" />
+      </section>
+    </section>
+  </article>
+</template>
